8056593aea6540119c4a413c9d7d7c56251099cf
RentalCat <theshootingstar1988@gmail.com>
2021-02-04 13:02:40 +0900

    zsh: fix lazyload anyenv

--- zsh/rc/anyenv.zsh
+++ zsh/rc/anyenv.zsh
@@ -1,36 +1,38 @@
 # anyenv
-if [[ ! -x `whence -p anyenv` ]] then
-  git clone https://github.com/riywo/anyenv ~/.anyenv
-fi
 function anyenv() {  # for lazyload
   unfunction "$0"
+  if [[ ! -x `whence -p anyenv` ]] then
+    git clone https://github.com/riywo/anyenv ~/.anyenv
+  fi
   source <(anyenv init -)
   $0 "$@"
 }
 
 
 # pyenv
-if [[ ! -x `whence -p pyenv` ]] then
-  anyenv install pyenv
-fi
-# pyenv virtualenv
-pyenv_path="$(pyenv root)/plugins/pyenv-virtualenv"
-if [[ ! -e $pyenv_path ]] then
-  git clone https://github.com/pyenv/pyenv-virtualenv.git $pyenv_path
-fi
 function pyenv() {  # for lazyload
   unfunction "$0"
+  anyenv &> /dev/null  # anyenvを遅延読込してパスを通す
+  if [[ ! -x `whence -p pyenv` ]] then
+    anyenv install pyenv
+  fi
+  # pyenv virtualenv
+  pyenv_path="$(pyenv root)/plugins/pyenv-virtualenv"
+  if [[ ! -e $pyenv_path ]] then
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $pyenv_path
+  fi
   source <(pyenv virtualenv-init -)
   $0 "$@"
 }
 
 
 # rbenv
-if [[ ! -x `whence -p rbenv` ]] then
-  anyenv install rbenv
-fi
 function rbenv() {  # for lazyload
   unfunction "$0"
+  anyenv &> /dev/null  # anyenvを遅延読込してパスを通す
+  if [[ ! -x `whence -p rbenv` ]] then
+    anyenv install rbenv
+  fi
   source <(rbenv init -)
   $0 "$@"
 }
